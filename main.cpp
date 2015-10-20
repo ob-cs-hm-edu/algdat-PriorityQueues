f45332d4276f945160fbff6b0b42aa76abf57ad5
Oliver Braun <ob@obraun.net>
2015-10-20 12:32:42 +0200

    add simple doxygen config file and documentation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+/**
+ * \brief Eine main-Funktion, die die Priority Queues willkommen heisst.
+ */
 int main() {
     cout << "Hello, Priority Queues!" << endl;
     return 0;
